33395dc2839e36dbdc400089677da0293521ed9d
A.H.M Sakif Shale Dhrubo <sakifdhrubo1997@gmail.com>
2024-06-30 03:52:20 +0600

    Version 1.3.1

--- app/src/main/java/com/example/alzeimier/Patient_Logical_Test.java
+++ app/src/main/java/com/example/alzeimier/Patient_Logical_Test.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Patient_Logical_Test extends AppCompatActivity {
     private ImageButton ext;
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_logical_test);
         ext=findViewById(R.id.imageButton6);
+        submit=findViewById(R.id.button18);
         ext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 eit();
             }
         });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sbmit();
+            }
+        });
 
     }
     public void eit(){
+        Intent b= new Intent(this,PatientHomePage.class);
+        startActivity(b);
+    }
+    public void sbmit(){
         Intent b= new Intent(this,PatientHomePage.class);
         startActivity(b);
     }
--- app/src/main/java/com/example/alzeimier/Patient_Word.java
+++ app/src/main/java/com/example/alzeimier/Patient_Word.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Patient_Word extends AppCompatActivity {
 
     private ImageButton ext;
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_word);
         ext=findViewById(R.id.imageButton5);
+        submit=findViewById(R.id.button20);
         ext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 et();
             }
         });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sbmit();
+            }
+        });
     }
     public void et(){
+        Intent a= new Intent(this, PatientHomePage.class);
+        startActivity(a);
+    }
+    public void sbmit(){
         Intent a= new Intent(this, PatientHomePage.class);
         startActivity(a);
     }
